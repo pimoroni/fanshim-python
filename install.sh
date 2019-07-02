c3e175c0e7f67c84de091c71e6df9a00f6c04450
Phil Howard <phil@gadgetoid.com>
2019-07-02 12:40:26 +0100

    Sync install from boilerplate

--- install.sh
+++ install.sh
@@ -1,22 +1,52 @@
 #!/bin/bash
 
-eval `python - <<EOF
+CONFIG=/boot/config.txt
+DATESTAMP=`date "+%Y-%M-%d-%H-%M-%S"`
+CONFIG_BACKUP=false
+
+CONFIG_VARS=`python - <<EOF
 from configparser import ConfigParser
 c = ConfigParser()
 c.read('library/setup.cfg')
-a = dict(c['pimoroni'])
-a.update(c['metadata'])
+p = dict(c['pimoroni'])
+# Convert multi-line config entries into bash arrays
+for k in p.keys():
+    fmt = '"{}"'
+    if '\n' in p[k]:
+        p[k] = "'\n\t'".join(p[k].split('\n')[1:])
+        fmt = "('{}')"
+    p[k] = fmt.format(p[k])
 print("""
 LIBRARY_NAME="{name}"
 LIBRARY_VERSION="{version}"
-PY3_DEPS="{py3deps}"
-PY2_DEPS="{py2deps}"
-""".format(**a))
+""".format(**c['metadata']))
+print("""
+PY3_DEPS={py3deps}
+PY2_DEPS={py2deps}
+SETUP_CMDS={commands}
+CONFIG_TXT={configtxt}
+""".format(**p))
 EOF`
+
+if [ $? -ne 0 ]; then
+	printf "Error parsing configuration...\n"
+	exit 1
+fi
+
+function do_config_backup {
+	if [ ! $CONFIG_BACKUP == true ]; then
+		CONFIG_BACKUP=true
+		FILENAME="config.preinstall-$LIBRARY_NAME-$DATESTAMP.txt"
+		printf "Backing up $CONFIG to $FILENAME\n"
+		cp $CONFIG $FILENAME
+	fi
+}
 
 function apt_pkg_install {
 	PACKAGES=()
-	for PACKAGE in "$@"; do
+	PACKAGES_IN=("$@")
+	for ((i = 0; i < ${#PACKAGES_IN[@]}; i++)); do
+		PACKAGE="${PACKAGES_IN[$i]}"
 		printf "Checking for $PACKAGE\n"
 		dpkg -L $PACKAGE > /dev/null 2>&1
 		if [ "$?" == "1" ]; then
@@ -31,6 +61,8 @@ function apt_pkg_install {
 	fi
 }
 
+eval $CONFIG_VARS
+
 printf "$LIBRARY_NAME $LIBRARY_VERSION Python Library: Installer\n\n"
 
 if [ $(id -u) -ne 0 ]; then
@@ -41,15 +73,35 @@ fi
 cd library
 
 printf "Installing for Python 2..\n"
-apt_pkg_install $PY2_DEPS
+apt_pkg_install "${PY2_DEPS[@]}"
 python setup.py install
 
 if [ -f "/usr/bin/python3" ]; then
 	printf "Installing for Python 3..\n"
-	apt_pkg_install $PY3_DEPS
+	apt_pkg_install "${PY3_DEPS[@]}"
 	python3 setup.py install
 fi
 
 cd ..
+
+for ((i = 0; i < ${#SETUP_CMDS[@]}; i++)); do
+	CMD="${SETUP_CMDS[$i]}"
+	# Attempt to catch anything that touches /boot/config.txt and trigger a backup
+	if [[ "$CMD" == *"raspi-config"* ]] || [[ "$CMD" == *"$CONFIG"* ]] || [[ "$CMD" == *"\$CONFIG"* ]]; then
+		do_config_backup
+	fi
+	eval $CMD
+done
+
+for ((i = 0; i < ${#CONFIG_TXT[@]}; i++)); do
+	CONFIG_LINE="${CONFIG_TXT[$i]}"
+	if ! [ "$CONFIG_LINE" == "" ]; then
+		do_config_backup
+		sed -i 's/^#$CONFIG_LINE/$CONFIG_LINE/' $CONFIG
+		if ! grep -q -E "^$CONFIG_LINE" $CONFIG; then
+			printf "$CONFIG_LINE\n" >> $CONFIG
+		fi
+	fi
+done
 
 printf "Done!\n"
