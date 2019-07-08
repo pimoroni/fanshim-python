2fe7f55ad6d16804addaf66473cc28457d4d27e2
Phil Howard <phil@gadgetoid.com>
2019-07-08 10:38:57 +0100

    Improve install-service.sh

--- examples/install-service.sh
+++ examples/install-service.sh
@@ -1,13 +1,60 @@
 #!/bin/bash
-THRESHOLD=${1:-36}
-HYSTERESIS=${2:-2}
-DELAY=${3:-2}
-PREEMPT=${4:-false}
+THRESHOLD=36
+HYSTERESIS=2
+DELAY=2
+PREEMPT="no"
 SERVICE_PATH=/etc/systemd/system/pimoroni-fanshim.service
 
-if $PREEMPT; then
+while [[ $# -gt 0 ]]; do
+	K="$1"
+	case $K in
+		-p|--preempt)
+		if [ "$2" == "yes" ] || [ "$2" == "no"]; then
+			PREEMPT="$2"
+			shift
+		else
+			PREEMPT="yes"
+		fi
+		shift
+		;;
+		-t|--threshold)
+		THRESHOLD="$2"
+		shift
+		shift
+		;;
+		-h|--hysteresis)
+		HYSTERESIS="$2"
+		shift
+		shift
+		;;
+		-d|--delay)
+		DELAY="$2"
+		shift
+		shift
+		;;
+		*)
+		shift
+	esac
+done
+
+if [ "$PREEMPT" == "yes" ]; then
     DO_PREEMPT='--preempt'
 fi
+
+cat << EOF
+Setting up with:
+Threshold: $THRESHOLD C
+Hysteresis: $HYSTERESIS C
+Delay: $DELAY seconds
+Preempt: $PREEMPT
+
+To change these options, run:
+sudo ./install-service --threshold <n> --hysteresis <n> --delay <n> (--preempt)
+
+Or edit: $SERVICE_PATH
+
+
+EOF
 
 read -r -d '' UNIT_FILE << EOF
 [Unit]
@@ -24,12 +71,20 @@ Restart=on-failure
 WantedBy=multi-user.target
 EOF
 
-echo "Installing psutil"
-pip3 install psutil fanshim
+printf "Checking for psutil\n"
+python3 - <<EOF
+import psutil
+EOF
+if [ $? -ne 0 ]; then
+	printf "Installing psutil\n"
+	pip3 install psutil fanshim
+else
+	printf "psutil already installed\n"
+fi
 
-echo "Installing service to: $SERVICE_PATH"
+printf "\nInstalling service to: $SERVICE_PATH\n"
 echo "$UNIT_FILE" > $SERVICE_PATH
 systemctl daemon-reload
-systemctl enable pimoroni-fanshim.service
-systemctl start pimoroni-fanshim.service
-systemctl status pimoroni-fanshim.service
+systemctl enable --no-pager pimoroni-fanshim.service
+systemctl start --no-pager pimoroni-fanshim.service
+systemctl status --no-pager pimoroni-fanshim.service
